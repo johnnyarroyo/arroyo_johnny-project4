fdf47b3ffcda94abe9da407f70df2c77dba17e5d
Johnny Arroyo <johnnyarroyo@fullsail.edu>
2011-09-22 23:41:32 -0400

    added rest of comments

--- arroyo_johnny_project1.js
+++ arroyo_johnny_project1.js
@@ -1,9 +1,9 @@
-//Validate a phone number
+//Validate a phone number.
 function formValidator()
 {
 
 
-   if(isNumeric(phone, "Please enter a valid phone number"))
+   if(isNumeric(phone, "Please enter a valid phone number"))//must meet length requirement and only be numbers.
    {
       if(lengthRestriction(phone, 10,10))
       {
@@ -15,7 +15,7 @@ function formValidator()
 
 
 function isNumeric(phone, helperMsg){
-    var numericExpression = /^[0-9]+$/;
+    var numericExpression = /^[0-9]+$/;//couldn't figure out how to add the dashes and where they should be places
     if(phone.value.match(numericExpression)){
         return true;
     }else{
@@ -29,28 +29,28 @@ function lengthRestriction(phone, min, max){
     if(uInput.length >= min && uInput.length <= max){
         return true;
     }else{
-        alert("Please enter between " +min+ " and " +max+ " characters");
+        alert("Please enter"  + max + " characters");//String concatination!
         return false;
     }
 }
 
 //Adding only the numbers in a string
-var yankees = ["awesome", 100, "best", 62, true]
+var yankees = ["awesome", 100, "best", 62, true]//sorry this was the best i could figure out.
 var totalGames = yankees[1] + yankees[3]
 console.log(totalGames)
 
 //How many day until thanksgiving?!
-today=new Date()
+today=new Date()//this one was pretty hard for me, I had to look up this function because I couldn't get it, but after doing so, I do understand it.
 var thanksgiving=new Date(today.getFullYear(), 10, 24) 
 if (today.getMonth()==10 && today.getDate()>24) 
 	birthday.setFullYear(thanksgiving.getFullYear()+1) 
-	var one_day=1000*60*60*24
+	var one_day=1000*60*60*24//(never would of thought to do this!!)
 	console.log(Math.ceil((thanksgiving.getTime()-today.getTime())/(one_day))+
 	" days left until Thanksgiving!!");
 	
 //How to validate email
-function emailValidator(elem, helperMsg){
-	var emailExp = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+function emailValidator(elem, helperMsg){	//I had help with this one, coudln't figure out how to validate using just JS but here is the script.
+	var emailExp = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;// I had to look up the regular expression because I couldn't figure it out.
 	if(elem.value.match(emailExp)){
 		return true;
 	}else{
@@ -62,16 +62,16 @@ function emailValidator(elem, helperMsg){
 
 //How to set number of decimal points
 var tesNumber = 343427.1264827
+console.log(tesNumber.fixed(2));
 
 //How to get number that is inside a certain percent of another number
-console.log(tesNumber.fixed(2));
 
 var x = 2;
 var y= 10;
 var gp = .35;
 var p = x/y;
 if (gp > p) {
-	console.log("x falls within");
+	console.log("x falls within 35% of y!");
 } else {
 	return false;
 };
@@ -101,7 +101,7 @@ numvar = parseInt(testString);
 
 console.log(numvar);
 
-//How to title-case a string. I got help with this one but couldn't figure out how to check it in javascript.
+//How to title-case a string. I got help with this one but couldn't figure out how to check it in javascript. I used a input field in the html to verify
 /*
 <html>
 <head>
@@ -126,7 +126,7 @@ document.getElementById("p1").style.textTransform="capitalize";
 function validate(form) { 
 	var form = "http://www.facebook.com";
     var v = new RegExp(); 
-    v.compile("^[A-Za-z]+://[A-Za-z0-9-_]+\\.[A-Za-z0-9-_%&\?\/.=]+$"); 
+    v.compile("^[A-Za-z]+://[A-Za-z0-9-_]+\\.[A-Za-z0-9-_%&\?\/.=]+$");//I needed help with this too. I get the concept of reg expressions but still have trouble putting them together.
     if (!v.test(form["URL"].value)) { 
         alert("You must supply a valid URL."); 
         return false; 
